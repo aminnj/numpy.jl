d89102a8124a0770bb3328432f508fa7d5d3285b
Nick Amin <amin.nj@gmail.com>
2021-08-16 12:27:47 -0700

    sorry

---
+++ README.md
@@ -0,0 +1,24 @@
+# numpy.jl
+
+First: sorry.
+
+For those times when Python muscle memory kicks in, or
+when you paste code into the wrong REPL...
+
+## Installation
+
+```julia
+]add https://github.com/aminnj/numpy.jl
+```
+
+## Usage
+
+```julia
+julia> import numpy as np
+
+julia> a = np.array([1,2,3,4])
+array([1, 2, 3, 4])
+
+julia> a.mean()
+2.5
+```
---
+++ src/numpy.jl
@@ -0,0 +1,28 @@
+module numpy
+
+using Base
+
+struct NDArray{T}
+    x::T
+end
+
+function Base.getproperty(a::NDArray, s::Symbol) 
+    x = Base.getfield(a, :x)
+    if s == :x x
+    elseif s == :sum return ()->sum(x)
+    elseif s == :min return ()->minimum(x)
+    elseif s == :max return ()->max(x)
+    elseif s == :mean return ()->sum(x)/length(x)
+    else error("not implemented")
+    end
+end
+
+function Base.show(io::IO, a::NDArray)
+    print(io, "array(")
+    print(io, a.x)
+    print(io, ")")
+end
+
+array(a) = NDArray(collect(a))
+
+end
