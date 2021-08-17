cdd9d211f3c8e1a8b9b0f92167975bf92b1fe5c7
Nick Amin <amin.nj@gmail.com>
2021-08-17 06:02:39 -0700

    two dim

--- src/numpy.jl
+++ src/numpy.jl
@@ -1,26 +1,48 @@
 module numpy
 
-using Base
+using Base, Statistics
 
 struct NDArray{T}
     x::T
 end
 
+function wrapper(func, x; axis)
+    if axis isa Int
+        axis += 1
+    end
+    out = func(x, dims=axis)
+    if ndims(out) > 1
+        out = array(out)
+    end
+    out
+end
+
 function Base.getproperty(a::NDArray, s::Symbol) 
     x = Base.getfield(a, :x)
     if s == :x x
-    elseif s == :sum return ()->sum(x)
-    elseif s == :min return ()->minimum(x)
-    elseif s == :max return ()->maximum(x)
-    elseif s == :mean return ()->sum(x)/length(x)
+    elseif s == :sum return (;axis=:)->wrapper(sum, x; axis)
+    elseif s == :min return (;axis=:)->wrapper(minimum, x; axis)
+    elseif s == :max return (;axis=:)->wrapper(maximum, x; axis)
+    elseif s == :mean return (;axis=:)->wrapper(mean, x; axis)
+    elseif s == :std return (;axis=:)->wrapper(std, x; axis)
+    elseif s == :T return NDArray(x')
     else error("not implemented")
     end
 end
 
 function Base.show(io::IO, a::NDArray)
-    print(io, "array(")
-    print(io, a.x)
-    print(io, ")")
+    if ndims(a.x) == 1
+        print(io, "array(")
+        print(io, a.x)
+        print(io, ")")
+    else
+        lines = split(repr(MIME"text/plain"(), a.x), '\n')[2:end]
+        print(io, "array([")
+        for (i,line) in enumerate(lines)
+            i == 1 ? print(io, line) : print(io, "       " * line)
+            i == length(lines) ?  print(io, "])") : println(io)
+        end
+    end
 end
 
 array(a) = NDArray(collect(a))
